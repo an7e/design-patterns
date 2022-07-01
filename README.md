850396120bb791a673ee77f841492242bddfb428
an7e <an7e@outlook.com>
2022-07-01 19:00:36 +0000

    add eslint

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # design-patterns
+
 Design Patterns implemented in TypeScript
+
+## Behavioral patterns
+
+## Creational patterns
+
+## Structural patterns
---
+++ src/creational/decorator.ts
@@ -0,0 +1,11 @@
+interface IUserService {
+    users: number
+    getUsersInDatabase(): number
+}
+
+class UserSerice implements IUserService {
+    users: number
+    getUsersInDatabase(): number {
+        throw new Error("Method not implemented.")
+    }
+}
