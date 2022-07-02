1ae9f5d6039cede7642d238db9a044752cbfe686
an7e <an7e@outlook.com>
2022-07-02 13:29:13 +0000

    add decorator

--- src/creational/decorator.ts
+++ src/creational/decorator.ts
@@ -4,8 +4,23 @@ interface IUserService {
 }
 
 class UserSerice implements IUserService {
-    users: number;
+    users = 1000;
+
     getUsersInDatabase(): number {
-        throw new Error('Method not implemented.');
+        return this.users;
     }
 }
+
+function nullUser(obj: IUserService) {
+    obj.users = 0;
+    return obj;
+}
+
+function logUsers(obj: IUserService) {
+    console.log('Users: ' + obj.users);
+    return obj;
+}
+
+console.log(new UserSerice().getUsersInDatabase());
+console.log(nullUser(new UserSerice()).getUsersInDatabase());
+console.log(logUsers(nullUser(new UserSerice())).getUsersInDatabase());
