61f284ef8a334c0d736ae68553fd2556d35156b3
an7e <an7e@outlook.com>
2022-07-03 15:11:37 +0000

    add decorator parameter

---
+++ src/creational/decorator-parameter.ts
@@ -0,0 +1,23 @@
+export interface IUserService {
+    getUsersInDatabase(): void;
+}
+
+export class UserSerice implements IUserService {
+    private _users: number;
+
+    getUsersInDatabase(): number {
+        return this._users;
+    }
+
+    setUsersInDatabase(@Positive() num: number): void {
+        this._users = num;
+    }
+}
+
+export function Positive() {
+    return (target: Object, propertyKey: string, parameterIndex: number) => {
+        console.log(target, propertyKey, parameterIndex);
+    };
+}
+
+const userSerice = new UserSerice();
